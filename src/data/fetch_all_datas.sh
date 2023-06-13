ace8737d4cebac24a3435b3c12821e441c5a61c7
AbdiHaryadi <abdiharyadi.ah@gmail.com>
2023-06-13 09:27:44 +0700

    feat: update fetch_all_datas.sh from https://github.com/feliciagojali/sentence-imp/blob/master/src/data/fetch_data.sh

--- src/data/fetch_all_datas.sh
+++ src/data/fetch_all_datas.sh
@@ -3,19 +3,22 @@
 DATA_PATH="./data"
 RAW_PATH="./raw"
 FEATURES_PATH="./features"
-RESULTS_PATH = "./results"
+RESULTS_PATH="./results"
+
+if [ ! -d $DATA_PATH ]; then
+  mkdir -p $DATA_PATH
+fi
 
 cd $DATA_PATH
 
-# if [ ! -d $RAW_PATH ]; then
-#   mkdir -p $RAW_PATH
-# fi
+if [ ! -d $RAW_PATH ]; then
+  mkdir -p $RAW_PATH
+fi
 
-# cd $RAW_PATH
+cd $RAW_PATH
 gdown --id 1wE9wJM4v1nE4-1aSz2MudOhANkl4rc73
 
-# cd ".."
-
+cd ".."
 
 if [ ! -d $FEATURES_PATH ]; then
   mkdir -p $FEATURES_PATH
@@ -38,3 +41,21 @@ cd ".."
 if [ ! -d $RESULTS_PATH ]; then
   mkdir -p $RESULTS_PATH
 fi
+
+PRETRAINED_PATH="./pretrained"
+
+if [ ! -d $PRETRAINED_PATH ]; then
+  mkdir -p $PRETRAINED_PATH
+fi
+
+cd $PRETRAINED_PATH
+
+echo "Downloading pretrained models"
+gdown --id 1553_9shAUrQpFAB0vqXbQrpHqJvWYgp4
+unzip word2vec-input_sent.txt-s300-c5-w5-e10-SG.model.trainables.syn1neg.zip
+rm word2vec-input_sent.txt-s300-c5-w5-e10-SG.model.trainables.syn1neg.zip
+gdown --id 1vL8vyfJbaj3i91peTu738jq25N-yhsU3
+gdown --id 1MQjcRLBCJsdk3AyCBWfAkltzRTHhI9ED
+unzip word2vec_news.model.wv.vectors.zip
+rm word2vec_news.model.wv.vectors.zip
+cd ".."
